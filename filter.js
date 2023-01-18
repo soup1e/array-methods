918ade08585dd7ca2b9e7a965311dc3b15b3007e
Sam Adams <sam.adams7785@gmail.com>
2023-01-18 10:17:32 -0800

    filtering test passing

--- filter.js
+++ filter.js
@@ -2,7 +2,7 @@
 // OUTPUT: only countries in Oceana
 // REQS: use .filter
 
-export const filterOceania = (countries) => {};
+export const filterOceania = (countries) => countries.filter((au) => au.continent === 'Oceania');
 
 // INPUT: the array of countries from data.js
 // OUTPUT: only countries whose name that include the letters 'au'
@@ -15,9 +15,9 @@ export const filterAu = (countries) => {
 // INPUT: the array of dogs from data.js
 // OUTPUT: only dogs that are 2 years old or younger
 // REQS: use .filter
-export const filterYoungDogs = (dogs) => {};
+export const filterYoungDogs = (dogs) => dogs.filter((dog) => dog.age <= 2);
 
 // INPUT: the array of dogs from data.js
 // OUTPUT: a list of names of dogs that are 2 years old or younger
 // REQS: use .filter AND .map OR use your filterYoungDogs function and a map
-export const getYoungDogNames = (dogs) => {};
+export const getYoungDogNames = (dogs) => dogs.filter((dog) => dog.age <= 2).map((dog) => dog.name);
