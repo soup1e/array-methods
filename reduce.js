57c615da9ff52332063647ce3ba27c080dc56a40
Sam Adams <sam.adams7785@gmail.com>
2023-01-18 10:34:05 -0800

    reduce test passing

--- reduce.js
+++ reduce.js
@@ -2,9 +2,9 @@
 // OUTPUT: an array of all the sneakers brands
 // REQS: use .reduce
 
-export const getSneaks = (brands) => {};
+export const getSneaks = (brands) => brands.reduce((all, brand) => [...all, ...brand.shoes], []);
 
 // INPUT: the array of dogs from data.js
 // OUTPUT: the average age of the dogs
 // REQS: use .reduce
-export const getAverageAge = (dogs) => {};
+export const getAverageAge = (dogs) => dogs.reduce((all, dog) => all + dog.age, 0) / dogs.length;
